001d53d8b6fe4d2e453677a7acd5b158bd7d3e1b
Will Harris <will.t.harris@protonmail.com>
2020-09-06 22:11:05 -0700

    feat: use fetchGraphQl and display GH data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {fetchGraphQl} from './fetchGraphQl'
+
+const {useState, useEffect} = React;
 
 function App() {
+  const [name, setName] = useState(null)
+
+  useEffect(() => {
+    let isMounted = true
+    fetchGraphQl(`
+      query RepoNameQuery {
+        repository(owner: "will-t-harris" name: "space-jam") {
+          name
+        }
+      }
+    `).then(response => {
+      if (!isMounted) {
+        return;
+      }
+      const data = response.data
+      setName(data.repository.name)
+    }).catch(error => {
+      console.error(error)
+    })
+
+    return () => {
+      isMounted = false;
+    }
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {name != null ? `Repository: ${name}` : "Loading"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
